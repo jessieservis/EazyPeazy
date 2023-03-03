efe88e172d05226b60542304dc2cee1b859aa578
Cepholapod <64045445+Cepholapod@users.noreply.github.com>
2023-03-02 23:40:46 -0500

    modified 'Meal'

--- src/Meal.java
+++ src/Meal.java
@@ -6,33 +6,12 @@ import java.util.ArrayList;
  * DATE
  */
 public class Meal implements Comparable <Meal> {
-	private String name; 
-	private double calories;
-	private static ArrayList<Float> nutritionValue = new ArrayList<Float>();
-	private ArrayList<String> labels = new ArrayList<String>();
+	double calories; 
 	
-	/**
-	 * 
-	 */
-	public Meal(String name, double calories) {
-		this.name = name; 
+	public Meal (double calories) {
 		this.calories = calories; 
-		
 	}
-	
-	public double getCalories(int count) {
-		
-		double totalCalories = 0;
-		//iterate through meal to count calories
-		for (int i = 0; i< count; i++) {
-			
-		}
-		
-		return totalCalories;
-	}
-	/*
-	 * compares meals based on calores. 
-	 */
+
 	public int compareTo(Meal other) {
 		return Double.compare(this.calories, other.calories);
 	}
