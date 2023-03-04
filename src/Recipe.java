c81ae9c55a2a059b35c64464c60c2996b524607c
Cepholapod <64045445+Cepholapod@users.noreply.github.com>
2023-03-04 16:56:23 -0500

    Minor updates

--- src/Recipe.java
+++ src/Recipe.java
@@ -18,42 +18,59 @@ import com.github.cliftonlabs.json_simple.Jsoner;
  * so if you are reading this then know that if this 
  * doesn't work out. we will go back to the og plan
  */
-public interface Recipe {
+public class Recipe {
 	
-	String Title(String html);
-	List<String> ingredients (String html);
-	List<String> Directions (String html);
-	Map<String, String> ingredientsWithQuantities(String html);
+	String title; 
+	List<String> Ingredients;
+	List<String> Directions ;
+	List<String> Tag;
+	Map<String, Double> Nutrition;
+	int servings;
+	String prepTime;
+	String cookTime;
 	
-	enum Keys implements JsonKey{
-		Title("title"),
-		Tags("tags"),
-		PrepTime("Prep_time"),
-		Servings("Servings"),
-		Ingredients("ingredients"),
-		Nutrition("nutrition"),
-		Directions("Directions");
-		
-		private final String key;
-		//private final Object value;
-		
-		Keys(String key){
-			this.key = key;
-			
-		}
-		
-		
-		
-		public String getKey() {
-			return key; 
-		}
-
-
-		public Object getValue() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
+	
+	public Recipe (String title, List<String> Ingredients, List<String> Directions,
+				Map<String,Double> Nutrition, List<String> Tag, String prepTime,
+				String CookTime) {
+		this.title = title; 
+		this.Directions = Directions;
+		this.Ingredients = Ingredients;
 	}
+	
+	public void setTitle(String title) {
+		this.title = title; 
+	}
+	
+	public void setIngredients (List<String> ingredients) {
+		this.Ingredients = ingredients; 
+	}
+	
+	public void setDirections(List<String> directions) {
+		this.Directions = directions;
+	}
+	
+	public void setTag (List<String> tag) {
+		this.Tag = tag;
+	}
+	
+	public void setNutrition (Map<String, Double> nutrition) {
+		this.Nutrition = nutrition; 
+	}
+	
+	public void setServing (int serving) {
+		this.servings = serving; 
+	}
+	
+	public void setCootTime (String cookTime) {
+		this.cookTime = cookTime;
+	}
+	
+	public void setPrepTime (String prepTime) {
+		this.prepTime = prepTime;
+	}
+	
+	
+
 	
 }
--- src/RecipeParser.java
+++ src/RecipeParser.java
@@ -1,34 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
-import java.util.Map;
-
+import java.util.NoSuchElementException;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonKey;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsonable;
+import com.github.cliftonlabs.json_simple.Jsoner;
 import javax.swing.text.Document;
-
 import org.jsoup.*;
+import com.github.cliftonlabs.json_simple.JsonArray;
 
+/**
+ * 
+ * @author sethm
+ * DESCRIPTION: takes recipe objects from json file and parses them 
+ */
+public class RecipeParser  {
+		
+	String file = "Recipes.json";
 
-public class RecipeParser implements Recipe {
-	
-	String title; 
-	
-	public Map<String, String> ingredientsWithQuantities(String htm){
-		return null;
-	}
-	public List<String>ingredients (String html){
+	public void jsonToArray(File f) {
+		JsonObject recipe = new JsonObject();
 		
-		return null; 
-	}
-	
-	public List<String> Directions (String html){
-		
-		
-		return null; 
-	}
-	
-	public String Title(String html) {
-		
-		org.jsoup.nodes.Document doc = Jsoup.parse(html);
-		
-		return doc.select("h1.headline").text(); 
+		try {
+			
+		}
+		catch(NoSuchElementException e){
+			e.printStackTrace();
+		}
 	}
 	
 	
